7d9fded108d62a54426433f17e72348e01abbe5d
arunkbabu90 <arunkbabu90@gmail.com>
2020-11-02 18:46:34 +0530

    Added MovieActivity.kt

---
+++ app/src/main/java/arunkbabu90/filimibeat/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package arunkbabu90.filimibeat.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import arunkbabu90.filimibeat.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
