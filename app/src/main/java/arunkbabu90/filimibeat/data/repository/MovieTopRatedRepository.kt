9e4a4f2f921a6f11d2ab4abba5a07f71e6f04bb9
FEBIN JACS GEORGE <febinjacs96@gmail.com>
2020-11-23 23:08:04 +0530

    Implemented TopRatedMovieRepository

---
+++ app/src/main/java/arunkbabu90/filimibeat/data/repository/MovieTopRatedRepository.kt
@@ -0,0 +1,29 @@
+package arunkbabu90.filimibeat.data.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import arunkbabu90.filimibeat.data.api.PAGE_SIZE
+import arunkbabu90.filimibeat.data.api.TMDBInterface
+import arunkbabu90.filimibeat.data.database.Movie
+import io.reactivex.disposables.CompositeDisposable
+
+class MovieTopRatedRepository(private val apiService: TMDBInterface) {
+    private lateinit var movieDataSourceFactory: TopRatedMovieDataSourceFactory
+
+    fun fetchTopRatedMovies(disposable: CompositeDisposable): LiveData<PagedList<Movie>> {
+        movieDataSourceFactory = TopRatedMovieDataSourceFactory(apiService, disposable)
+
+        val config = PagedList.Config.Builder()
+                .setPageSize(PAGE_SIZE)
+                .setEnablePlaceholders(false)
+                .build()
+
+        return LivePagedListBuilder(movieDataSourceFactory, config).build()
+    }
+
+    fun getNetworkState(): LiveData<NetworkState> {
+        return Transformations.switchMap(movieDataSourceFactory.topRatedMovieList, TopRatedMovieDataSource::networkState)
+    }
+}
