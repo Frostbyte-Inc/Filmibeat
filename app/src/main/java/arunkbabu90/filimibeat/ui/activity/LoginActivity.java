acbd8a4919eb403deb1878b6cc24ab36ee76508d
FEBIN JACS GEORGE <febinjacs96@gmail.com>
2020-11-24 23:41:57 +0530

    Fully Implemented TopRatedMovie

---
+++ app/src/main/java/arunkbabu90/filimibeat/ui/activity/LoginActivity.java
@@ -0,0 +1,13 @@
+package arunkbabu90.filimibeat.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
