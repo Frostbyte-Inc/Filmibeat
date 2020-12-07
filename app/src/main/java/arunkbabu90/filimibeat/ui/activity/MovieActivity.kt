2fbd985e44fd78d7c403afaacbe2649920759130
arunkbabu90 <arunkbabu90@gmail.com>
2020-12-08 01:17:36 +0530

    Added Toolbar

--- app/src/main/java/arunkbabu90/filimibeat/ui/activity/MovieActivity.kt
+++ app/src/main/java/arunkbabu90/filimibeat/ui/activity/MovieActivity.kt
@@ -3,59 +3,39 @@ package arunkbabu90.filimibeat.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import arunkbabu90.filimibeat.Constants
 import arunkbabu90.filimibeat.R
+import arunkbabu90.filimibeat.databinding.ActivityMovieBinding
 import arunkbabu90.filimibeat.ui.adapter.CategoryAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_movie.*
 
 class MovieActivity : AppCompatActivity() {
     private var tabLayoutMediator: TabLayoutMediator? = null
+    private lateinit var binding: ActivityMovieBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie)
+        binding = ActivityMovieBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         window.statusBarColor = ActivityCompat.getColor(this, R.color.colorPurpleDark)
         window.navigationBarColor = ActivityCompat.getColor(this, R.color.colorPurple)
 
         // Set the title of the tabs
         tabLayoutMediator = TabLayoutMediator(movie_tab_layout, movie_view_pager) { tab, position ->
-            tab.text = setTabTitle(position)
+            tab.text = when (position) {
+                0 -> getString(R.string.tab_title_now_playing)
+                1 -> getString(R.string.tab_title_popular)
+                2 -> getString(R.string.tab_title_top_rated)
+                3 -> getString(R.string.tab_title_search)
+                else -> ""
+            }
         }
 
-        val pageCount = if (Constants.userType == Constants.USER_TYPE_PERSON) 5 else 4
-        val categoryAdapter = CategoryAdapter(supportFragmentManager, lifecycle, pageCount)
+        val categoryAdapter = CategoryAdapter(supportFragmentManager, lifecycle)
         movie_view_pager.adapter = categoryAdapter
         movie_view_pager.offscreenPageLimit = 4
 
         tabLayoutMediator?.attach()
-    }
-
-    /**
-     * Helper method to add or remove tab title based on userType
-     * @param position The tab position
-     */
-    private fun setTabTitle(position: Int): String {
-        return if (Constants.userType == Constants.USER_TYPE_PERSON) {
-            // Normal User
-            when (position) {
-                0 -> getString(R.string.tab_title_now_playing)
-                1 -> getString(R.string.tab_title_popular)
-                2 -> getString(R.string.tab_title_top_rated)
-                3 -> getString(R.string.tab_title_search)
-                4 -> getString(R.string.tab_title_favourites)
-                else -> ""
-            }
-        } else {
-            // Other users; Guest
-            when (position) {
-                0 -> getString(R.string.tab_title_now_playing)
-                1 -> getString(R.string.tab_title_popular)
-                2 -> getString(R.string.tab_title_top_rated)
-                3 -> getString(R.string.tab_title_search)
-                else -> ""
-            }
-        }
     }
 }
--- app/src/main/java/arunkbabu90/filimibeat/ui/adapter/CategoryAdapter.kt
+++ app/src/main/java/arunkbabu90/filimibeat/ui/adapter/CategoryAdapter.kt
@@ -4,44 +4,24 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import arunkbabu90.filimibeat.Constants
-import arunkbabu90.filimibeat.ui.fragment.*
+import arunkbabu90.filimibeat.ui.fragment.NowPlayingFragment
+import arunkbabu90.filimibeat.ui.fragment.PopularFragment
+import arunkbabu90.filimibeat.ui.fragment.SearchFragment
+import arunkbabu90.filimibeat.ui.fragment.TopRatedFragment
 import org.jetbrains.annotations.NotNull
 
-class CategoryAdapter(@NotNull fm: FragmentManager, @NotNull lifecycle: Lifecycle,
-                      private val pageCount: Int)
+class CategoryAdapter(@NotNull fm: FragmentManager, @NotNull lifecycle: Lifecycle)
     : FragmentStateAdapter(fm, lifecycle) {
 
-    override fun getItemCount(): Int = pageCount
+    override fun getItemCount(): Int = 4
 
     override fun createFragment(position: Int): Fragment {
-        return setFragmentsBasedOnUserType(position)
-    }
-
-    /**
-     * Helper method to set the fragments to the pager based on the userType
-     * @param position The tab position
-     */
-    private fun setFragmentsBasedOnUserType(position: Int): Fragment {
-        return if (Constants.userType == Constants.USER_TYPE_PERSON) {
-            // Normal User
-            when (position) {
-                0 -> NowPlayingFragment()
-                1 -> PopularFragment()
-                2 -> TopRatedFragment()
-                3 -> SearchFragment()
-                4 -> FavouritesFragment()
-                else -> NowPlayingFragment()
-            }
-        } else {
-            // Other Users; Guest
-            when (position) {
-                0 -> NowPlayingFragment()
-                1 -> PopularFragment()
-                2 -> TopRatedFragment()
-                3 -> SearchFragment()
-                else -> NowPlayingFragment()
-            }
+        return when (position) {
+            0 -> NowPlayingFragment()
+            1 -> PopularFragment()
+            2 -> TopRatedFragment()
+            3 -> SearchFragment()
+            else -> NowPlayingFragment()
         }
     }
 }
