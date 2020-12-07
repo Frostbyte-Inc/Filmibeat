45c62832531886cbe0b97a2582ce6fc0f3fff7ee
arunkbabu90 <arunkbabu90@gmail.com>
2020-12-08 03:18:02 +0530

    Implemented Sign Out Functionality

--- app/src/main/java/arunkbabu90/filimibeat/ui/activity/MovieActivity.kt
+++ app/src/main/java/arunkbabu90/filimibeat/ui/activity/MovieActivity.kt
@@ -2,6 +2,7 @@ package arunkbabu90.filimibeat.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import arunkbabu90.filimibeat.Constants
@@ -9,16 +10,24 @@ import arunkbabu90.filimibeat.R
 import arunkbabu90.filimibeat.databinding.ActivityMovieBinding
 import arunkbabu90.filimibeat.ui.adapter.CategoryAdapter
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_movie.*
 
-class MovieActivity : AppCompatActivity() {
-    private var tabLayoutMediator: TabLayoutMediator? = null
+class MovieActivity : AppCompatActivity(), FirebaseAuth.AuthStateListener {
     private lateinit var binding: ActivityMovieBinding
+    private lateinit var auth: FirebaseAuth
+
+    private var tabLayoutMediator: TabLayoutMediator? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMovieBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
+        auth.addAuthStateListener(this)
 
         window.statusBarColor = ActivityCompat.getColor(this, R.color.colorPurpleDark)
         window.navigationBarColor = ActivityCompat.getColor(this, R.color.colorPurple)
@@ -44,6 +53,7 @@ class MovieActivity : AppCompatActivity() {
         binding.toolbarMain.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.mnu_favourites -> {
+                    // Favourites
                     if (Constants.userType == Constants.USER_TYPE_PERSON) {
                         // Safety Check to prevent opening Favourites if the user is a Guest
                         val favIntent = Intent(this, FavouritesActivity::class.java)
@@ -53,8 +63,14 @@ class MovieActivity : AppCompatActivity() {
                 }
 
                 R.id.mnu_profile_name -> {
+                    // Profile
                     val profIntent = Intent(this, ProfileActivity::class.java)
                     startActivity(profIntent)
+                    true
+                }
+                R.id.mnu_sign_out -> {
+                    // Sign Out
+                    auth.signOut()
                     true
                 }
                 else -> false
@@ -62,5 +78,15 @@ class MovieActivity : AppCompatActivity() {
         }
         val menu = if (Constants.userType == Constants.USER_TYPE_PERSON) R.menu.main_menu_user else R.menu.main_menu_guest
         binding.toolbarMain.inflateMenu(menu)
+    }
+
+    override fun onAuthStateChanged(p0: FirebaseAuth) {
+        // User is either signed out or the login credentials no longer exists. So launch the login
+        // activity again for the user to sign-in
+        if (auth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            Toast.makeText(this, getString(R.string.signed_out), Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 }
