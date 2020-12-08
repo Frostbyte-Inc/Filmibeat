365a7e9f27d22e1ed50727c706dca4a3c1b93a95
arunkbabu90 <arunkbabu90@gmail.com>
2020-12-09 00:25:54 +0530

    Added an Extension Function to resize bitmaps

--- app/src/main/java/arunkbabu90/filimibeat/ui/activity/ProfileActivity.kt
+++ app/src/main/java/arunkbabu90/filimibeat/ui/activity/ProfileActivity.kt
@@ -2,25 +2,32 @@ package arunkbabu90.filimibeat.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import arunkbabu90.filimibeat.R
+import arunkbabu90.filimibeat.databinding.ActivityProfileBinding
 import arunkbabu90.filimibeat.ui.adapter.ProfileAdapter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 
 class ProfileActivity : AppCompatActivity() {
-    private lateinit var adapter: ProfileAdapter
-    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityProfileBinding
     private lateinit var firebaseStorage: FirebaseStorage
+    private lateinit var adapter: ProfileAdapter
     private lateinit var db: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
 
-    private var isViewsLoaded: Boolean = false
     private var dpPath = ""
     private var fullName = ""
     private var email = ""
 
+    companion object {
+        var isUpdatesAvailable = false
+        private const val REQUEST_CODE_PICK_IMAGE = 2000
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
