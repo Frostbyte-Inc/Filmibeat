d49f0ddf65700cb1d730a96a9f8372a5360ac47c
FEBIN JACS GEORGE <febinjacs96@gmail.com>
2020-12-09 20:12:54 +0530

    action card

--- app/src/main/java/arunkbabu90/filimibeat/ui/view/ActionCard.kt
+++ app/src/main/java/arunkbabu90/filimibeat/ui/view/ActionCard.kt
@@ -1,25 +1,21 @@
 package arunkbabu90.filimibeat.ui.view
 
 import android.content.Context
-import android.content.res.TypedArray
 import android.util.AttributeSet
-import androidx.annotation.StyleRes
+import android.widget.ImageView
+import android.widget.TextView
 import arunkbabu90.filimibeat.R
 import com.google.android.material.card.MaterialCardView
 
-class ActionCard @JvmOverloads constructor(context: Context, attr:AttributeSet?=null, defStyleAttr: int = 0, defStyleRes: StyleRes)
-    : MaterialCardView(context,attr,defStyleAttr){
-        private var iconResourceid=-1
-        private  var cardDescriptionText = ""
-    init {
-        val a: TypedArray = context.theme.obtainStyledAttributes(attr, R.styleable.ActionCard,defStyleAttr, defStyleRes: 0)
-        try {
-            iconResourceid = a.getResourceId(R.styleable.ActionCard_icon, defvalue:-1)
-            cardDescriptionText = a.getString(R.styleable.ActionCard_description) ?: ""
+class ActionCard@JvmOverloads constructor(context: Context, attr: AttributeSet, defStyleAttributeSet: int = 0)
+    : MaterialCardView(context, attr, defStyleAttributeSet){
+        init {
+            inflate(context, R.layout.vm_action_card,this)
+            val imageView : ImageView = findViewById(R.id.action_card_icon_view)
+            val textView : TextView = findViewById(R.id.action_card_desc)
+            val attributes = context.obtainStyledAttributes(attr, R.styleable.ActionCard)
+            imageView.setImageDrawable(attributes.getDrawable(R.styleable.ActionCard_icon))
+            textView.text = attributes.getString(R.styleable.ActionCard_description)
+            attributes.recycle()
         }
-finally {
-    a.recycle()
-}
-        val inflater context.getStystemService(context.LAYOUT_INFLATER_SERVICE) as LayoutInfalter
-    }
     }
