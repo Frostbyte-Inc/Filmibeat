02098e28795087156326c3f30c114e8c7e1a770a
arunkbabu90 <arunkbabu90@gmail.com>
2020-12-15 14:25:46 +0530

    Optimized Status Bar in FavouritesActivity

--- app/src/main/java/arunkbabu90/filimibeat/ui/activity/FavouritesActivity.kt
+++ app/src/main/java/arunkbabu90/filimibeat/ui/activity/FavouritesActivity.kt
@@ -2,11 +2,14 @@ package arunkbabu90.filimibeat.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import arunkbabu90.filimibeat.R
 
 class FavouritesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favourites)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.colorPurpleDark)
     }
 }
