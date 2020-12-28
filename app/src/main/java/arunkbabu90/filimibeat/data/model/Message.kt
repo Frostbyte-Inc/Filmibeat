6ac2f31b5490c455b040d1df46506b01f5b2ad81
arunkbabu90 <arunkbabu90@gmail.com>
2020-12-28 22:44:11 +0530

    Add Message Model

---
+++ app/src/main/java/arunkbabu90/filimibeat/data/model/Message.kt
@@ -0,0 +1,17 @@
+package arunkbabu90.filimibeat.data.model
+
+/**
+ * Data Object representing a single Message in the Chat Room
+ */
+data class Message(var key: String = "",
+                   val msg: String = "",
+                   val senderId: String = "",
+                   val receiverId: String = "",
+                   val status: Int = -1,
+                   val msgTimestamp: Long = -1) {
+
+    companion object {
+        const val TYPE_YOU = 4000
+        const val STATUS_SEND = 1
+    }
+}
